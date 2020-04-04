909786cecc3a64d7be723c9cc963654006f4d0ee
KevinBetterQ <kevinbetterq@gmail.com>
2020-04-04 22:31:02 +0800

    ok with chinese

--- sample1/main.go
+++ sample1/main.go
@@ -8,11 +8,14 @@ import (
 	"GoInAction/sample1/search"
 )
 
+// init is called prior to main.
 func init() {
 	log.SetOutput(os.Stdout)
 }
 
+// main is the entry point for the program.
 func main() {
 	// 使用特定的项做搜索
-	search.Run("test")
+	// Perform the search for the specified term.
+	search.Run("president")
 }
